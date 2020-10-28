441588d467d56f6917de8f9dc31ce94d52392235
Danny Vega <dannyvega13579@gmail.com>
2020-10-28 12:29:03 -0700

    primer commit - prueba de NodeJS

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+var app = express();
+
+app.set("view engine", "pug");
+
+app.get("/", function(req,res){
+    res.render("index", {hola: "Hola Danny"});
+}); 
+
+app.listen(8080);
